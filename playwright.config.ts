146ef6574a0952e0a52d7ad12ac5aeb732f93850
Enrico9009 <enricoguazzini90@gmail.com>
2022-12-04 21:31:39 +0700

    removed redundant code

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,13 +3,6 @@ import { PlaywrightTestConfig } from '@playwright/test';
 import { testConfig } from './testConfig';
 const ENV = process.env.ENV;
 
-// if (!ENV || ![`UI`].includes(ENV)) {
-//   console.log(
-//     `Please provide a correct environment value like "npx cross-env ENV=UI"`
-//   );
-//   process.exit();
-// }
-
 const config: PlaywrightTestConfig = {
   expect: {
     timeout: 10000,
@@ -28,7 +21,6 @@ const config: PlaywrightTestConfig = {
   projects: [
     {
       name: `MailMerge`,
-      // grepInvert: /@UI/,
       use: {
         browserName: `chromium`,
         channel: `chrome`,
